446b77d12b9275a8da054919389d128678f058bf
ahmadparvej <ahmadparvej58@gmail.com>
2022-05-29 09:16:52 +0530

    added production build

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,15 +7,17 @@ const Header = () => {
   return (
     <header>
       <div className="container header__container">
-        <h5>Hello I'm</h5>
-        <h1>Parvej Ahmad</h1>
-        <h5 className="text-light">Full-Stack Developer</h5>
-        <CTA/>
-        <HeaderSocials/>
+        <div className="iAm">
+          <h5>Hello I'm</h5>
+          <h1>Parvej Ahmad</h1>
+          <h5 className="text-light">Full-Stack Developer</h5>
+          <CTA/>
+          <HeaderSocials/>
+        </div>
         <div className="me">
           <img src={ME} alt="me" />
         </div>
-        <a href="#contact" className="scroll__down">scroll down</a>
+        {/* <a href="#contact" className="scroll__down">scroll down</a> */}
       </div>
     </header>
   )
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { BsLinkedin } from "react-icons/bs";
 import { BsGithub } from "react-icons/bs";
-import { BsDribbble } from "react-icons/bs";
+import { BsTwitter } from "react-icons/bs";
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="https://www.linkedin.com/in/parvej-ahmad-627674207/" target="_blank"><BsLinkedin/></a>
-        <a href="https://github.com/ahmadparvej" target="_blank"><BsGithub/></a>
-        <a href="https://www.linkedin.com/in/parvej-ahmad-627674207/" target="_blank"><BsDribbble/></a>
+        <a href="https://www.linkedin.com/in/parvej-ahmad-627674207/" target="_blank" rel="noreferrer"><BsLinkedin/></a>
+        <a href="https://github.com/ahmadparvej" target="_blank" rel="noreferrer"><BsGithub/></a>
+        <a href="https://twitter.com/codemagzine" target="_blank" rel="noreferrer"><BsTwitter/></a>
     </div>
   )
 }
