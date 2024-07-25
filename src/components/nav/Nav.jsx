c3ae9f9e2e92fb98a678c06210888503679ecc51
ahmadparvej <ahmadparvej58@gmail.com>
2024-07-25 09:13:16 +0530

    update navigation tooltip

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,21 +1,55 @@
-import React,{useState} from 'react'
-import './nav.css'
-import { AiOutlineHome,AiOutlineUser} from "react-icons/ai";
-import { BiBook,BiMessageDetail } from "react-icons/bi";
+import React, { useState } from "react";
+import "./nav.css";
+import { AiOutlineHome, AiOutlineUser } from "react-icons/ai";
+import { BiBook, BiMessageDetail } from "react-icons/bi";
 import { RiServiceLine } from "react-icons/ri";
 
-
 const Nav = () => {
-  const [active, setActive] = useState('home')
+  const [active, setActive] = useState("home");
   return (
     <nav>
-      <a href="#" className={active==="home"?'active':""} onClick={()=>setActive("home")}><AiOutlineHome/></a>
-      <a href="#about" className={active==="about"?'active':""} onClick={()=>setActive("about")}><AiOutlineUser/></a>
-      <a href="#experience" className={active==="experience"?'active':""} onClick={()=>setActive("experience")}><RiServiceLine/></a>
-      <a href="#portfolio" className={active==="portfolio"?'active':""} onClick={()=>setActive("portfolio")}><BiBook/></a>
-      <a href="#contact" className={active==="contact"?'active':""} onClick={()=>setActive("contact")}><BiMessageDetail/></a>
+      <a
+        href="#"
+        className={active === "home" ? "active" : ""}
+        onClick={() => setActive("home")}
+        title="Home"
+      >
+        <AiOutlineHome />
+      </a>
+      <a
+        href="#about"
+        className={active === "about" ? "active" : ""}
+        onClick={() => setActive("about")}
+        title="About"
+      >
+        <AiOutlineUser />
+      </a>
+      <a
+        href="#experience"
+        className={active === "experience" ? "active" : ""}
+        onClick={() => setActive("experience")}
+        title="Experience"
+      >
+        <RiServiceLine />
+      </a>
+      <a
+        href="#portfolio"
+        className={active === "portfolio" ? "active" : ""}
+        onClick={() => setActive("portfolio")}
+        title="Portfolio"
+      >
+        <BiBook />
+      </a>
+      <a
+        href="#contact"
+        className={active === "contact" ? "active" : ""}
+        onClick={() => setActive("contact")}
+        title="Contact"
+      >
+        <BiMessageDetail />
+      </a>
     </nav>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
