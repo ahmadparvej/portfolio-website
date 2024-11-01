ae7a67702086e10df44c9dd49ad9bec632d1f936
ahmadparvej <ahmadparvej58@gmail.com>
2024-11-01 09:51:28 +0530

    designation change

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,8 @@ const Header = () => {
         <div className="iAm">
           <h5>Hello I'm</h5>
           <h1>Parvej Ahmad</h1>
-          <h5 className="text-light">Software Engineer at Cumulus System (Hitachi Group) | Full-Stack Web Developer</h5>
+          <h5 className="text-light">Full-Stack Web Developer</h5>
+          <h5 className="text-light">Worked as a Software Engineer at Hitachi</h5>
           <CTA/>
           <HeaderSocials/>
         </div>
